6f78286567fcaea0a0b18857d52bc25a6c93b5da
Yan Kravchenko <yan@yanfosec.com>
2024-06-03 11:51:32 -0500

    Content Update

--- copy-content.sh
+++ copy-content.sh
@@ -1,5 +1,7 @@
 
 rm -rf /Users/h68836/Library/CloudStorage/OneDrive-Personal/Projects/yanfosec.github.io/content/*
 cp -r /Users/h68836/Library/Mobile\ Documents/iCloud\~md\~obsidian/Documents/PPRC\ Notes/* /Users/h68836/Library/CloudStorage/OneDrive-Personal/Projects/yanfosec.github.io/content
-
-
+cd /Users/h68836/Library/CloudStorage/OneDrive-Personal/Projects/yanfosec.github.io/
+git add .
+git commit -m "Content Update"
+git push origin main
